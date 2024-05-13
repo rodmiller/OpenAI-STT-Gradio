bc6accd4b2f959b3337edbe52ae394f5e3f3ca33
Rob Miller <flyingcrank@gmail.com>
2024-05-13 22:34:39 +0100

    Update

--- main.py
+++ main.py
@@ -31,13 +31,16 @@ def transcript(audio, model, response_type):
 
     return transcriptions
 
+def process(output_text, process_type):
+
+
 
 def upload_file(files):
     print(files)
 
 
 with gr.Blocks() as demo:
-    gr.Markdown("# <center> OpenAI Speed To Text API with Gradio </center>")
+    gr.Markdown("# <center> OpenAI Speech To Text API with Gradio </center>")
     with gr.Row(variant="panel"):
         model = gr.Dropdown(choices=["whisper-1"], label="Model", value="whisper-1")
         response_type = gr.Dropdown(choices=["json", "text", "srt", "verbose_json", "vtt"], label="Response Type",
@@ -49,7 +52,14 @@ with gr.Blocks() as demo:
 
     output_text = gr.Text(label="Output Text")
 
+    process_type = gr.Dropdown(choices=["Referral", "Clinic Letter", "Correspondence Letter"], label="Process Type", value="process_type")
+
+    process_button = gr.Button(value="Process")
+
+    processed_text = gr.Text(label="Processed Text")
+
     audio.stop_recording(fn=transcript, inputs=[audio, model, response_type], outputs=output_text, api_name=False)
     file.upload(fn=transcript, inputs=[file, model, response_type], outputs=output_text)
+    process_button.click(fn=process, inputs=[output_text, process_type], outputs=processed_text)
 
 demo.launch()
