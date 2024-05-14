211c30cdfcfb8894520f78d36f76df6e85e47023
Rob Miller <flyingcrank@gmail.com>
2024-05-14 07:37:29 +0100

    Update

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def process(output_text, process_type):
 def save_result(processed_result):
 	timestamp = datetime.now().replace(microsecond=0).isoformat()
 	with open("/home/robert/vaults/obsidian/001 Inbox/"+timestamp+".md", "w") as output_file:
-		
+		output_file.write(frontmatter)
 		output_file.write(processed_result)
 	return output_file
 
