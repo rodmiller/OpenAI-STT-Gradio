d615d9215bfafa5ad2a5d8da5a4cc446cdb65e02
Rob Miller <flyingcrank@gmail.com>
2024-05-25 10:37:47 +0100

    Try just listing the recordings dir

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def streamingChange(checkbox_value):
 	return gr.Audio(streaming=checkbox_value)
 
 def get_recording_files():
-	return [f for f in os.listdir("./recordings") if os.path.isfile(os.path.join("./recordings", f))]
+	return os.listdir("./recordings")
 
 
 with gr.Blocks(head=shortcut_js) as demo:
